e72d26b9ce704487d05d19ea4978dec1c624d194
John-Vincent Saddic <js873514@wcupa.edu>
2020-10-27 19:01:50 -0400

    Player Script Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+
+    public Vector3 startPos;
+    public Rigidbody RB;
+
+
+    //Go to starting position
+    void GoToStart()
     {
-        
+        transform.position = startPos;
+        RB.velocity = Vector3.zero;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+        GoToStart();
+    }
+
     void Update()
     {
-        
+        if (transform.position.y < -20)
+            GoToStart();
     }
 }

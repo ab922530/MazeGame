254fb24fa6ee69c27e56b30856b5e815e56c1691
John-Vincent Saddic <js873514@wcupa.edu>
2020-10-30 13:23:10 -0400

    Playerprefs built

---
+++ Assets/Scripts/Highscore.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Highscore : MonoBehaviour
+{
+    public static int highestLevel = 0;
+    public static float fastestTime = 200000f;
+
+    void Awake()
+    {
+        // If the PlayerPrefs Highest Level already exists, read it
+        if (PlayerPrefs.HasKey("Highest Level"))
+            highestLevel = PlayerPrefs.GetInt("Highest Level");
+
+        // If the PlayerPrefs fastest already exists, read it
+        if (PlayerPrefs.HasKey("Fastest Time"))
+            highestLevel = PlayerPrefs.GetInt("Fastest Time");
+
+        // Assign the high scores to Highest Level and Fastest Time
+        PlayerPrefs.SetInt("Highest Level", highestLevel);
+        PlayerPrefs.SetFloat("Fastest Time", fastestTime);
+    }
+
+    public static void CheckScoreBeaten()
+    {
+        int level = MazeGame.level;
+        float time = MazeGame.S.time;
+        if (level <= PlayerPrefs.GetInt("Highest Level"))
+        {
+            PlayerPrefs.SetInt("Highest Level", highestLevel);
+            if (time < PlayerPrefs.GetFloat("Fastest Time"))
+                PlayerPrefs.SetFloat("Fastest Time", time);
+        }
+
+        highestLevel = PlayerPrefs.GetInt("Highest Level");
+        fastestTime = PlayerPrefs.GetInt("Fastest Time");
+    }
+}
